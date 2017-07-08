49e43d030d30725759faadbfb4dc4e90495330a1
chrism3027 <chris.m.mcclure@gmail.com>
2017-07-07 22:25:31 -0500

    initial commit

---
+++ AspNetCoreVideo/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreVideo.Controllers
+{
+ 
+    public class EmployeeController : Controller
+    {
+        public EmployeeController()
+        {
+
+        }
+
+        public ContentResult Name()
+        {
+            return Content("Chris Rules");
+        }
+        public string Country()
+        {
+            return "Chris wants to visit Sweden";
+        }
+        public string Index()
+        {
+            return "Hello from Employee";
+        }
+    }
+}
